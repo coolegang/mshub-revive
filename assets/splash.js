e8fe565ca1b817427121b8cb13a513d08303af56
69Chr1s69 <149000825+69Chr1s69@users.noreply.github.com>
2023-10-26 23:01:59 +0200

    .

--- assets/splash.js
+++ assets/splash.js
@@ -2,7 +2,13 @@ const splashes = `
 designed by maxxus and chris
 "He has my full respect, he made us a full website, he's working hard, give him a raise" - chris
 "He's the best guy ever" - chris
-mogus
+amogus
+WE LIVE WE LOVE WE LIE!!!!!
+look behind you
+never gonna give you up never gonna let you down HAHA
+911 what's you're emergency
+my brain isn't braining
+we die we hate we truth
 `.split('\n'); splashes.splice(0, 1); splashes.splice(splashes.length - 1, 1);
 // stuff after split() is so it doesnt think the empty space after/before the ` is a splash
 
